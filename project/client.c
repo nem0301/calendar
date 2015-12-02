1e15729aa5999369dc1762b6a60855519d2659c7
nem0301 <sks156324@gmail.com>
2015-12-02 22:05:31 +0900

    "/send" command is added and some codes are relocated. part of command is relocated to "handle_clnt" from "send_meg". client is modified in " because "/send" command is added.

--- project/client.c
+++ project/client.c
@@ -5,91 +5,113 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <pthread.h>
-	
+
 #define BUF_SIZE 257
 #define NAME_SIZE 20
-	
+
 void * send_msg(void * arg);
 void * recv_msg(void * arg);
 void error_handling(char * msg);
-	
-char name[NAME_SIZE]="[DEFAULT]";
-char msg[BUF_SIZE];
-	
-int main(int argc, char *argv[])
-{
-	int sock;
-	struct sockaddr_in serv_addr;
-	pthread_t snd_thread, rcv_thread;
-	void * thread_return;
 
-	if(argc!=4) {
-		printf("Usage : %s <IP> <port> <name>\n", argv[0]);
-		exit(1);
-	}
-	
-	sprintf(name, "[%s]", argv[3]);
-	sock=socket(PF_INET, SOCK_STREAM, 0);
-	
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-	serv_addr.sin_port=htons(atoi(argv[2]));
-	  
-	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
-		error_handling("connect() error");
-	
-	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
-	pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock);
-	pthread_join(snd_thread, &thread_return);
-	pthread_join(rcv_thread, &thread_return);
-	close(sock);  
-	return 0;
+char name[NAME_SIZE] = "[DEFAULT]";
+char msg[BUF_SIZE];
+
+int main(int argc, char *argv[]) {
+  int sock;
+  struct sockaddr_in serv_addr;
+  pthread_t snd_thread, rcv_thread;
+  void * thread_return;
+
+  if (argc != 4) {
+    printf("Usage : %s <IP> <port> <name>\n", argv[0]);
+    exit(1);
+  }
+
+  sprintf(name, "[%s]", argv[3]);
+  sock = socket(PF_INET, SOCK_STREAM, 0);
+
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+  serv_addr.sin_port = htons(atoi(argv[2]));
+
+  if (connect(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
+    error_handling("connect() error");
+
+  pthread_create(&snd_thread, NULL, send_msg, (void*) &sock);
+  pthread_create(&rcv_thread, NULL, recv_msg, (void*) &sock);
+  pthread_join(snd_thread, &thread_return);
+  pthread_join(rcv_thread, &thread_return);
+  close(sock);
+  return 0;
 }
-	
+
 void * send_msg(void * arg)   // send thread main
 {
-	int sock=*((int*)arg);
-	char name_msg[NAME_SIZE+BUF_SIZE];
+  int sock = *((int*) arg);
+  char name_msg[NAME_SIZE + BUF_SIZE];
+  char* ptr;
+  char* token;
+  FILE* fp;
+  char sendbuf[BUF_SIZE];
+  char tempbuf[BUF_SIZE];
 
-	while(1) 
-	{
-		fgets(msg, BUF_SIZE, stdin);
-        //when input q or Q, then quit 
-		if(!strcmp(msg,"q\n")||!strcmp(msg,"Q\n")) 
-		{
-			close(sock);
-			exit(0);
-		}
-        
-        //send name and massage
-		sprintf(name_msg,"%s %s", name, msg);
-		write(sock, name_msg, strlen(name_msg));
-	}
-	return NULL;
+  while (1) {
+    fgets(msg, BUF_SIZE, stdin);
+
+    strcpy(tempbuf, msg);
+
+    token = strtok_r(tempbuf, " ", &ptr);
+
+    if(strcmp(token, "/send") == 0){
+      sprintf(name_msg, "%s %s", name, msg);
+      write(sock, name_msg, strlen(name_msg));
+
+      token = strtok_r(NULL, " ", &ptr);
+      token[strlen(token)-1] = '\0';
+
+      fp = fopen(token, "rb");
+
+      usleep(100);
+      while(fread(sendbuf, 1, BUF_SIZE, fp) > 0){
+        write(sock, sendbuf, BUF_SIZE);
+      }
+      fclose(fp);
+      continue;
+    }
+
+    //when input q or Q, then quit
+    if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n")) {
+      close(sock);
+      exit(0);
+    }
+
+    //send name and massage
+    sprintf(name_msg, "%s %s", name, msg);
+    write(sock, name_msg, strlen(name_msg));
+  }
+  return NULL;
 }
-	
+
 void * recv_msg(void * arg)   // read thread main
 {
-	int sock=*((int*)arg);
-	char name_msg[NAME_SIZE+BUF_SIZE];
-	int str_len;
+  int sock = *((int*) arg);
+  char name_msg[NAME_SIZE + BUF_SIZE];
+  int str_len;
 
-    //whenever client recieves a message from server, print to stdout
-	while(1)
-	{
-		str_len=read(sock, name_msg, NAME_SIZE+BUF_SIZE-1);
-		if(str_len==-1) 
-			return (void*)-1;
-        name_msg[str_len]=0;
-		fputs(name_msg, stdout);
-	}
-	return NULL;
+  //whenever client recieves a message from server, print to stdout
+  while (1) {
+    str_len = read(sock, name_msg, NAME_SIZE + BUF_SIZE - 1);
+    if (str_len == -1)
+      return (void*) -1;
+    name_msg[str_len] = 0;
+    fputs(name_msg, stdout);
+  }
+  return NULL;
 }
-	
-void error_handling(char *msg)
-{
-	fputs(msg, stderr);
-	fputc('\n', stderr);
-	exit(1);
+
+void error_handling(char *msg) {
+  fputs(msg, stderr);
+  fputc('\n', stderr);
+  exit(1);
 }
--- project/server.c
+++ project/server.c
@@ -16,206 +16,210 @@ void send_msg(char * msg, int len, int clnt_sock);
 void error_handling(char * msg);
 void execution(int clnt_sock);
 
-int clnt_cnt=0;
+int clnt_cnt = 0;
 int clnt_socks[MAX_CLNT];
 pthread_mutex_t mutx;
 pthread_mutex_t mutx_for_arg;
 
 char* args_g[10];
 
-int main(int argc, char *argv[])
-{
-	int serv_sock, clnt_sock;
-	struct sockaddr_in serv_adr, clnt_adr;
-	int clnt_adr_sz;
-	pthread_t t_id;
+int main(int argc, char *argv[]) {
+  int serv_sock, clnt_sock;
+  struct sockaddr_in serv_adr, clnt_adr;
+  int clnt_adr_sz;
+  pthread_t t_id;
 
+  if (argc != 2) {
+    printf("Usage : %s <port>\n", argv[0]);
+    exit(1);
+  }
 
-	if(argc!=2) {
-		printf("Usage : %s <port>\n", argv[0]);
-		exit(1);
-	}
+  //initialize mutex
+  pthread_mutex_init(&mutx, NULL);
+  pthread_mutex_init(&mutx_for_arg, NULL);
 
-    //initialize mutex
-	pthread_mutex_init(&mutx, NULL);
+  //open server socket, bind and listen
+  serv_sock = socket(PF_INET, SOCK_STREAM, 0);
 
-    //open server socket, bind and listen
-	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+  memset(&serv_adr, 0, sizeof(serv_adr));
+  serv_adr.sin_family = AF_INET;
+  serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv_adr.sin_port = htons(atoi(argv[1]));
 
-	memset(&serv_adr, 0, sizeof(serv_adr));
-	serv_adr.sin_family=AF_INET; 
-	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
-	serv_adr.sin_port=htons(atoi(argv[1]));
-	
-	if(bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr))==-1)
-		error_handling("bind() error");
-	if(listen(serv_sock, 5)==-1)
-		error_handling("listen() error");
-	
-    /* when client try to connect to server, server give one of clnt_socks.
-     * Next, server call handle_clnt thread and detach thread for returning resource
-     */ 
-	while(1)
-	{
-		clnt_adr_sz=sizeof(clnt_adr);
-		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr,&clnt_adr_sz);
-		
-		pthread_mutex_lock(&mutx);
-		clnt_socks[clnt_cnt++]=clnt_sock;
-		pthread_mutex_unlock(&mutx);
-	
-		pthread_create(&t_id, NULL, handle_clnt, (void*)&clnt_sock);
-		pthread_detach(t_id);
-		printf("Connected client IP: %s \n", inet_ntoa(clnt_adr.sin_addr));
-	}
-	close(serv_sock);
-	return 0;
+  if (bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr)) == -1)
+    error_handling("bind() error");
+  if (listen(serv_sock, 5) == -1)
+    error_handling("listen() error");
+
+  /* when client try to connect to server, server give one of clnt_socks.
+   * Next, server call handle_clnt thread and detach thread for returning resource
+   */
+  while (1) {
+    clnt_adr_sz = sizeof(clnt_adr);
+    clnt_sock = accept(serv_sock, (struct sockaddr*) &clnt_adr, &clnt_adr_sz);
+
+    pthread_mutex_lock(&mutx);
+    clnt_socks[clnt_cnt++] = clnt_sock;
+    pthread_mutex_unlock(&mutx);
+
+    pthread_create(&t_id, NULL, handle_clnt, (void*) &clnt_sock);
+    pthread_detach(t_id);
+    printf("Connected client IP: %s \n", inet_ntoa(clnt_adr.sin_addr));
+  }
+  close(serv_sock);
+  return 0;
 }
-	
-void* handle_clnt(void* arg)
-{
-    int clnt_sock = *(int*)arg;
-	int str_len=0, i;
-	char msg[BUF_SIZE];
-	
-    //waiting client message
-	while((str_len=read(clnt_sock, msg, sizeof(msg)))!=0){
-		send_msg(msg, str_len, clnt_sock);
+
+void* handle_clnt(void* arg) {
+  int clnt_sock = *(int*) arg;
+  int str_len = 0, i, j, k, n;
+  char msg[BUF_SIZE];
+  char* ptr;
+  char* token;
+  char buf[100];
+  char tempbuf[100];
+  char recvbuf[BUF_SIZE];
+  FILE* fp;
+
+  //waiting client message
+  while ((str_len = read(clnt_sock, msg, sizeof(msg))) != 0) {
+    strcpy(buf, msg);
+    buf[str_len - 1] = '\0';
+
+    token = strtok_r(buf, " ", &ptr);
+    token = strtok_r(NULL, " ", &ptr);
+    if (token == NULL)
+      continue;
+
+    //if first charater in the string is '/', then it is command string
+    if (strcmp("/exec", token) == 0) {
+      i = 0;
+      token = strtok_r(NULL, " ", &ptr);
+      pthread_mutex_lock(&mutx_for_arg);
+      while (token) {
+        args_g[i] = (char*) malloc(strlen(token));
+        strcpy(args_g[i], token);
+        token = strtok_r(NULL, " ", &ptr);
+        i++;
+      }
+
+      for (k = i; k < 10; k++)
+        args_g[k] = (char*) NULL;
+
+      execution(clnt_sock);
+
+      for (k = 0; k < i; k++) {
+        printf("%s\n", args_g[k]);
+        free(args_g[k]);
+      }
+      pthread_mutex_unlock(&mutx_for_arg);
+    } else if (strcmp("/send", token) == 0) {
+      printf("send\n");
+      token = strtok_r(NULL, " ", &ptr);
+      sprintf(tempbuf, "server is receiving %s\n", token);
+      write(clnt_sock, tempbuf, strlen(tempbuf));
+
+      sprintf(tempbuf, "%s_received", token);
+      printf("%s", tempbuf);
+      fp = fopen(tempbuf, "wb");
+
+      while ( (n = read(clnt_sock, recvbuf, BUF_SIZE)) > 0) {
+        fwrite(recvbuf, 1, BUF_SIZE, fp);
+        if( n < BUF_SIZE ){
+          break;
+        }
+      }
+
+      fclose(fp);
+    } else {
+      send_msg(msg, str_len, clnt_sock);
     }
+  }
 
-    printf("--------------------------\n");
-
-
-	//if client is disconnected, then remove client
-	pthread_mutex_lock(&mutx);
-	for(i=0; i<clnt_cnt; i++)   // remove disconnected client
-	{
-		if(clnt_sock==clnt_socks[i])
-		{
-			while(i++<clnt_cnt-1)
-				clnt_socks[i]=clnt_socks[i+1];
-			break;
-		}
-	}
-	clnt_cnt--;
-	pthread_mutex_unlock(&mutx);
-	close(clnt_sock);
-	return NULL;
+  //if client is disconnected, then remove client
+  pthread_mutex_lock(&mutx);
+  for (i = 0; i < clnt_cnt; i++) {   // remove disconnected client
+    if (clnt_sock == clnt_socks[i]) {
+      while (i++ < clnt_cnt - 1)
+        clnt_socks[i] = clnt_socks[i + 1];
+      break;
+    }
+  }
+  clnt_cnt--;
+  pthread_mutex_unlock(&mutx);
+  close(clnt_sock);
+  return NULL;
 }
+
 void send_msg(char * msg, int len, int clnt_sock)   // send to all
 {
-	int i, j, k;
-    
-    char* ptr[0];
-    char* token;
-    char buf[100];
-    pthread_t tid;
+  int i;
 
-    strcpy(buf, msg);
-    buf[len-1] = '\0';
-    //printf("send_msg : %s\n", buf);
-    
-    token = strtok_r(buf, " ", &ptr[0]);
-    token = strtok_r(NULL, " ", &ptr[0]);
-    //if first charater in the string is '/', then it is command string
-    if(strcmp("/exec", token) == 0){
-        i = 0;
-        token = strtok_r(NULL, " ", &ptr[0]);
-	    pthread_mutex_lock(&mutx_for_arg);
-        while(token){                
-            args_g[i] = (char*) malloc (strlen(token));           
-            strcpy(args_g[i], token);
-            token = strtok_r(NULL, " ", &ptr[0]);
-            i++;
-        }
+  //common conversation
+  //send a massage to all user not for sender
+  //if message have '/emotion' string, then it have to be changed to something
+  pthread_mutex_lock(&mutx);
+  for (i = 0; i < clnt_cnt; i++) {
+    if (clnt_sock == clnt_socks[i])
+      continue;
+    write(clnt_socks[i], msg, len);
+  }
+  pthread_mutex_unlock(&mutx);
 
-        for(k = i; k < 10; k++)
-            args_g[k] = (char*)NULL;        
+}
+void error_handling(char * msg) {
+  fputs(msg, stderr);
+  fputc('\n', stderr);
+  exit(1);
+}
 
-        execution(clnt_sock);
-        
-        for(k = 0; k < i; k++){
-            free(args_g[k]);
-        }
-	    pthread_mutex_unlock(&mutx_for_arg);
-    } else {
+void execution(int clnt_sock) {
+  pid_t pid;
+  int n, i;
+  char msg[257];
+  int pd;
+  int fd[2];
+  int status;
+  int flags;
+  int count = 0;
 
-        //common conversation
-        //send a massage to all user not for sender
-        //if message have '/emotion' string, then it have to be changed to something
-	    pthread_mutex_lock(&mutx);
-        for(i=0; i<clnt_cnt; i++){            
-            if (clnt_sock == clnt_socks[i])
-                continue;            
-            write(clnt_socks[i], msg, len);
-        }
-	    pthread_mutex_unlock(&mutx);
+  pipe(fd);
+
+  switch (pid = fork()) {
+  case -1:
+    perror("fork");
+    exit(1);
+  case 0:
+    close(1);
+    dup(fd[1]);
+    close(2);
+    dup(fd[1]);
+    execvp(args_g[0], args_g);
+    break;
+  default:
+    flags = fcntl(pd, F_GETFL, 0);
+    fcntl(fd[0], F_SETFL, flags | O_NONBLOCK);
+
+    while (waitpid(pid, &status, WNOHANG) == pid);
+    while (1) {
+      count++;
+      n = read(fd[0], msg, 256);
+      if (n < 0) {
+        usleep(100);
+        if (count > 100) {
+          break;
+        } else
+          continue;
+      } else {
+        msg[n] = '\0';
+        write(clnt_sock, msg, strlen(msg));
+        write(1, msg, strlen(msg));
+      }
+      if (n < 256) {
+        break;
+      }
     }
-
-
+    break;
+  }
 }
-void error_handling(char * msg)
-{
-	fputs(msg, stderr);
-	fputc('\n', stderr);
-	exit(1);
-}
-
-void execution(int clnt_sock){
-    pid_t pid;
-    int n, i;
-    char msg[257];
-    int pd;
-    int fd[2];
-    int status;
-    int flags;
-    int count = 0;
-
-    pipe(fd);    
-    
-    switch (pid = fork()){    
-        case -1:
-            perror("fork");
-            exit(1);
-        case 0:
-            close(1);
-            dup(fd[1]);
-            close(2);
-            dup(fd[1]);
-            execvp(args_g[0], args_g);
-            break;
-        default:            
-            flags = fcntl (pd, F_GETFL, 0);
-            fcntl(fd[0], F_SETFL, flags | O_NONBLOCK);
-
-            //while( waitpid(pid, &status, WNOHANG) == pid);
-            //while( (n = read(fd[0], msg, 256))  > 0){
-            while(1){
-                count++;
-                n = read(fd[0], msg, 256);
-                if ( n < 0){
-                    usleep(100);
-                    if ( count > 100){
-                        break;
-                    }
-                    else
-                        continue;
-                } else {
-                    msg[n] = '\0';
-                    write(clnt_sock, msg, strlen(msg));            
-                    write(1, msg, strlen(msg));
-                }               
-                if ( n < 256){
-                    break;
-                } 
-            } 
-        
-            //msg[n] = '\0';
-            //write(1, msg, strlen(msg));
-            //write(clnt_sock, msg, strlen(msg));
-            break;
-    }
-}
-
-
 
